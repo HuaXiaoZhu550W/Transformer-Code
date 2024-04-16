275e6d71196e51329fbf4147c90bfbc026bc99fc
花小猪 <110916688+HuaXiaoZhu550W@users.noreply.github.com>
2024-04-16 15:05:09 +0800

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -1,19 +1,24 @@
-import torch
-
-
 # 学习率下降策略(热身反平方衰减)
 class WarmupInverseSqrtDecay:
-    def __init__(self, optimizer, d_model, warmup_steps, steps):
-        super(WarmupInverseSqrtDecay, self).__init__()
+    def __init__(self, optimizer, d_model, warmup_steps):
+        """
+        初始化学习率调整器。
+        :param optimizer: 优化器对象
+        :param d_model: 模型的维度
+        :param warmup_steps: 热身步数
+        """
         self.optimizer = optimizer
-        self.d_model = torch.tensor(d_model, dtype=torch.float32)
+        self.d_model = d_model
         self.warmup_steps = warmup_steps
-        self.steps = float(steps+1)  # 当前step
+        self.step_num = 0  # 当前步数
 
     def step(self):
-        lr = (self.d_model ** -0.5) * min(self.steps ** -0.5, self.steps * (self.warmup_steps ** -1.5))
-        self.steps += 1.
-        # 更新学习率
-        for p in self.optimizer.param_groups:
-            p['lr'] = lr
+        """
+        更新学习率并增加步数。
+        :return: 当前学习率
+        """
+        self.step_num += 1
+        lr = (self.d_model ** -0.5) * min(self.step_num ** -0.5, self.step_num * (self.warmup_steps ** -1.5))
+        for param_group in self.optimizer.param_groups:
+            param_group['lr'] = lr
         return lr
